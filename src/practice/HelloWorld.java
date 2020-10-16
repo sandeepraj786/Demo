234b6653b533c840b6ee1dab265aa32231f277f6
SANDEEP RAJ <66646163+sandeepraj786@users.noreply.github.com>
2020-10-16 11:00:50 +0530

    Update HelloWorld.java
    changes the greeting message

--- src/practice/HelloWorld.java
+++ src/practice/HelloWorld.java
@@ -2,7 +2,7 @@ package practice;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Fellowship and learn and grow");
+		System.out.println("Welcome to Fellowship and learn and grow and take job");
 	}
 
 }
