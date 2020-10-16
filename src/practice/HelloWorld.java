1e10a85c4bd91438659e0ae81272fbff44f39e53
SANDEEP RAJ <66646163+sandeepraj786@users.noreply.github.com>
2020-10-16 10:51:45 +0530

    Update HelloWorld.java

--- src/practice/HelloWorld.java
+++ src/practice/HelloWorld.java
@@ -2,7 +2,7 @@ package practice;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Fellowship");
+		System.out.println("Welcome to Fellowship and learn");
 	}
 
 }
